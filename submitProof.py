c582b802db4026ae29d37e3e9bfc1820ad7c8f9a
elhung <165709015+elliekumo@users.noreply.github.com>
2025-07-19 12:06:52 -0700

    Add files via upload

--- submitProof.py
+++ submitProof.py
@@ -204,7 +204,9 @@ def send_signed_msg(proof, random_leaf):
     })
 
     # sign and send
-    signed_tx = w3.eth.account.sign_transaction(tx, private_key=acct.key)
+    # signed_tx = w3.eth.account.sign_transaction(tx, private_key=acct.key)
+    # tx_hash = w3.eth.send_raw_transaction(signed_tx.rawTransaction)
+    signed_tx = acct.sign_transaction(tx)
     tx_hash = w3.eth.send_raw_transaction(signed_tx.rawTransaction)
 
     print("Transaction sent! TX hash:", tx_hash.hex())
